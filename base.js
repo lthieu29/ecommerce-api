e6e61321f979a2d25bfef36888c337326f344959
lthieu29 <lhieu.dev@gmail.com>
2024-08-15 22:29:30 +0700

    Thi cong API shopee lay don hang chi tiet + quy ky

--- base.js
+++ base.js
@@ -72,4 +72,46 @@ class Ecommerce {
     getEscrowDetail(param) {
 
     }
+
+    /**
+     * Call dữ liệu sang sàn thương mại điện tử
+     */
+    callAPIEcommerce(url, method = 'GET', query = {}, headers = {}, body = null) {
+        const keys = Object.keys(query);
+        keys.forEach(key => {
+            if(url.includes("?")) {
+                url = `${url}&${key}=${query[key]}}`;
+            } else {
+                url = `${url}?${key}=${query[key]}`;
+            }
+        })
+        
+        const options = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers // Kết hợp các header tùy chỉnh vào
+            }
+        };
+    
+        // Nếu là phương thức POST, thêm body vào options
+        if (method === 'POST' && body) {
+            options.body = JSON.stringify(body);
+        }
+    
+        // Gọi API
+        return fetch(url, options)
+            .then(response => {
+                if (!response.ok) {
+                    showData(response);
+                }
+                return response.json(); // chuyển đổi dữ liệu response thành JSON
+            })
+            .then(data => {
+                showData(data);
+            })
+            .catch(error => {
+                showData(error);
+            });
+    }
 }
--- shopee.js
+++ shopee.js
@@ -1,4 +1,10 @@
 class Shopee extends Ecommerce {
+    constructor() {
+        this.partnerID = localStorage.getItem("ShoppePartnerID");
+        this.partnerSecret = localStorage.getItem("ShopeePartnerSecret");
+        this.host = "https://partner.shopeemobile.com/";
+    }
+
     /**
      * Lấy access token
      */
@@ -58,20 +64,77 @@ class Shopee extends Ecommerce {
      * Lấy thông tin chi tiết 1 đơn hàng
      */
     getOrderDetail(param) {
-
+        
     }
 
     /**
      * Lấy thông tin chi tiết nhiều đơn hàng
      */
     getOrderDetails(param) {
+        const endpoint = localStorage.getItem("GetOrderDetails");
 
+        if(!endpoint) {
+            showData("Chưa gán dữ liệu localStorage");
+            return;
+        }
+
+        if(param.AccessToken && param.ShopID && param.OrderSnList) {
+            const timestamp = (int)(new Date().getTime() / 1000);
+            const str = `${this.partnerID}${endpoint}${timestamp}${param.AccessToken}${param.ShopID}`;
+            const sign = generateHmacSHA256Signature(this.partnerSecret, str);
+
+            console.log("Sign: " + sign);
+            console.log("TimeStamp: " + timestamp);
+
+            this.callAPIEcommerce(`${this.host}${endpoint}`, 'GET', {
+                partner_id: this.partnerID,
+                timestamp: timestamp,
+                access_token: param.AccessToken,
+                shop_id: param.ShopID,
+                sign: sign,
+                order_sn_list: param.OrderSnList,
+                request_order_status_pending: true,
+                response_optional_fields: "buy_user_id,buyer_username,estimated_shipping_fee,recipient_address,actual_shipping_fee,goods_to_declare,note,note_update_time,item_list,pay_time,dropshipper,dropshipper_phone,split_up,buyer_cancel_reason,cancel_by,cancel_reason,actual_shipping_fee_confirmed,buyer_ccf_id,fulfillment_flag,pickup_done_time,package_list,shipping_carrier,Payment_method,total_amount,buyer_username,invoice_data,no_plastic_packing,order_chargeable_weight_gram,edt,region,currency,cod,pending_terms,order_status,message_to_seller,create_time,update_time,days_to_ship,ship_by_date,checkout_shipping_carrier,reverse_shipping_fee,prescription_check_status,update_time"
+            })
+        } else {
+            showData("Kiểm tra lại tham số đầu vào, truyền thiếu gì đó");
+        }
     }
 
     /**
      * Lấy ký sổ (riêng Shopee)
      */
     getEscrowDetail(param) {
+        const endpoint = localStorage.getItem("GetEscrowDetail");
 
+        if(!endpoint) {
+            showData("Chưa gán dữ liệu localStorage");
+            return;
+        }
+
+        if(param.AccessToken && param.ShopID && param.OrderSnList) {
+            const timestamp = (int)(new Date().getTime() / 1000);
+            const str = `${this.partnerID}${endpoint}${timestamp}${param.AccessToken}${param.ShopID}`;
+            const sign = generateHmacSHA256Signature(this.partnerSecret, str);
+
+            console.log("Sign: " + sign);
+            console.log("TimeStamp: " + timestamp);
+
+            this.callAPIEcommerce(`${this.host}${endpoint}`, 'POST', {
+                partner_id: this.partnerID,
+                timestamp: timestamp,
+                access_token: param.AccessToken,
+                shop_id: param.ShopID,
+                sign: sign
+            }, {}, {
+                order_sn_list: param.OrderSnList.split(",")
+            });
+        } else {
+            showData("Kiểm tra lại tham số đầu vào, truyền thiếu gì đó");
+        }
+    }
+
+    generateHmacSHA256Signature(secret, message) {
+        return CryptoJS.HmacSHA256(message, secret).toString(CryptoJS.enc.Hex);
     }
 }
